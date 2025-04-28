5b55615712ae85bfcafe20237cf777fed22ad11a
SHKim <57246360+shkim0824@users.noreply.github.com>
2025-04-28 20:32:31 +0900

    Create train_pipeline_example.py

---
+++ train_pipeline_example.py
@@ -0,0 +1,34 @@
+for epoch in range(epochs):
+    start_time = time.time()
+
+    rng, *keys = random.split(rng, 4)
+    train_loader = trn_loader_aug(rng=keys[0])
+    val_loader = vl_loader(rng=keys[1])
+    test_loader = tst_loader(rng=keys[2])
+
+    for batch in train_loader:
+        state, metrics = train_step(state, batch)
+    metrics_history['train_acc'].append(metrics['acc'])
+    metrics_history['train_loss'].append(metrics['loss'])
+
+    for batch in test_loader:
+        metrics = test_step(state, batch) # In test, we do not need key
+        for metric, value in metrics.items(): # compute metrics
+            metrics_history[f'test_{metric}'].append(value) # record metrics
+
+    epoch_time = time.time() - start_time
+    print(f"Epoch {epoch+1} in {epoch_time:.2f} sec")
+    print(f"Train acc: {100*metrics_history['train_acc'][-1]:.2f}% Test acc: {100*metrics_history['test_acc'][-1]:.2f}%")
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,4))
+ax1.set_title('Accuracy')
+ax2.set_title('NLL')
+
+ax1.plot(metrics_history['test_acc'], label='ResNet-32')
+ax2.plot(metrics_history['test_loss'], label='ResNet-32')
+
+ax1.legend()
+ax2.legend()
+plt.show()
+plt.clf()
